1b602ff53292d521063cebed74170c64e0c17214
bacti <hai.tranminh@gameloft.com>
2019-11-28 14:34:23 +0700

    +

---
+++ docs/precache-manifest.7a721acb2d790378f0d7b77de4431fa5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed1e9a7702a894ddd8170a3694c791a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "965cd7f1ce7cc3d18b10",
+    "url": "/static/css/main.997a493a.chunk.css"
+  },
+  {
+    "revision": "a7e81091acb213a972d8",
+    "url": "/static/js/2.59830c99.chunk.js"
+  },
+  {
+    "revision": "965cd7f1ce7cc3d18b10",
+    "url": "/static/js/main.626e7b71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
